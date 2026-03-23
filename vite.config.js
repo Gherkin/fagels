ee8f4546990231c621abf84e77a7f14e88eec225
niyohn <niyohn@stekligan.se>
2026-03-23 21:48:14 +0100

    added changes for anki cards

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    host: true
+  },
   base: './',
   build: {
     minify: false
